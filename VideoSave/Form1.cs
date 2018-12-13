4ec98cd665bb0311893220242d69307ca2494779
ShenYhan <hang940608@sina.cn>
2018-12-13 11:30:59 +0800

    file

---
+++ VideoSave/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+
+namespace VideoSave
+{
+    public partial class Form1 : Form
+    {
+        Capture capture_Iron;
+        private Mat frame_Iron;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            capture_Iron = new Capture("11.avi"); //打开视频、摄像头 
+            capture_Iron.ImageGrabbed += Process_Iron;
+            capture_Iron.Start();
+        }
+        private void Process_Iron(object sender, EventArgs e)
+        {
+            capture_Iron.Retrieve(frame_Iron, 0);
+            System.Threading.Thread.Sleep((int)(800.0 / 10 - 5));
+            imageBox1.Image = frame_Iron;
+        }
+    }
+}
